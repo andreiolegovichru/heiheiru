8092a6c690e034007a6a667803e7802fed8f09a4
heihei-admin <heihei.ru@yandex.ru>
2019-08-30 16:41:31 +0300

    some new folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
-/*import gulp from 'gulp';*/
-
-/*var gulp = require('gulp'); 
-
-*/
-
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require('gulp')
+,sass = require('gulp-sass'),
+postcss = require('gulp-postcss'),
+browserSync = require('browser-sync').create(),
+autoprefixer = require('autoprefixer');
 /*Do not need to require gulp-watch anymore*/
 /*const watch = require('gulp-watch'); */
 
@@ -17,13 +13,24 @@ gulp.task('default', function(done) {
 
 // compile scss into css
 
-function style() {
+function styles() {
+	console.log("style is running!");
+	// 1. where is my dev css files
+	return gulp.src('./app/styles/style.css')
+	// 2. pass that file through postcss compiller
+	.pipe(postcss([autoprefixer]))
+	// 3. where do I save the compiled CSS?
+	.pipe(gulp.dest('./app/temp/styles'))
+}
+
+function scssFunction() {
+	console.log("scssFunction is running!");
 	// 1. where is my scss file
 	return gulp.src('./scss/**/*.scss')
 	// 2. pass that file through sass compiller
 	.pipe(sass())
 	// 3. where do I save the compiled CSS?
-	.pipe(gulp.dest('./css'))
+	.pipe(gulp.dest('./app/css'))
 }
 
 /*gulp.task('watch', () => {
@@ -39,11 +46,12 @@ function html() {
 
 function watchFiles() {
 	gulp.watch("./app/index.html", html);
+	gulp.watch("./app/styles/**/*.css", styles);
 }
 
 
-
-exports.style = style;
+exports.scss = scssFunction;
+exports.styles = styles;
 exports.html = html;
 exports.watch = watchFiles;
 
